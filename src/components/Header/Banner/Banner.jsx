c13d13a382ad16667c1e5dbca8d5ffd73cad6e61
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-17 23:15:08 +0600

    added sliders in banner

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,11 +1,22 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
+import slider1 from "../../../assets/slider/slider-1.jpeg";
+import slider2 from "../../../assets/slider/slider-2.jpeg";
+import slider3 from "../../../assets/slider/slider-3.jpeg";
+import slider4 from "../../../assets/slider/slider-4.jpeg";
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="mt-12">
+      <Carousel>
+        <img className="rounded-xl" src={slider1} />
+        <img className="rounded-xl" src={slider2} />
+        <img className="rounded-xl" src={slider3} />
+        <img className="rounded-xl" src={slider4} />
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
--- src/layouts/MainLayouts/MainLayouts.jsx
+++ src/layouts/MainLayouts/MainLayouts.jsx
@@ -1,4 +1,5 @@
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Header/Banner/Banner";
 import Navbar from "../../components/Header/Navbar/Navbar";
 
 const MainLayouts = () => {
@@ -6,6 +7,7 @@ const MainLayouts = () => {
     <div className="max-w-[1400px] mx-auto mt-8">
       <header>
         <Navbar></Navbar>
+        <Banner></Banner>
       </header>
 
       <main></main>
