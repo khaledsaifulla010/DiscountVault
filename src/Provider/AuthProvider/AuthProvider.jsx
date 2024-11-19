7841ebe85aeb359bda83805bb4597d7e46b0d3af
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-19 16:11:45 +0600

    check current user using observer

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,17 +1,20 @@
 import {
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "firebase/auth";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../../firebase/firebase.init";
 
 export const AuthContext = createContext();
 
 const googleProvider = new GoogleAuthProvider();
-import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
   //Sign In with Google //
 
   const googleSignIn = () => {
@@ -30,10 +33,29 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  // Current User //
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        console.log("User Logged in", currentUser);
+        setUser(currentUser);
+      } else {
+        console.log("No User Logged in!");
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   const authInfo = {
     googleSignIn,
     signUp,
     logIn,
+    user,
   };
 
   return (
