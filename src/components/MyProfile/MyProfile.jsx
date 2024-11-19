d7de1c249ed7825789798ecb328a73258807ca31
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-19 20:39:34 +0600

    added user image name & email in my profile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,11 +1,36 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
 
 const MyProfile = () => {
-    return (
-        <div>
-            <h1>My Profile</h1>
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="mb-36 mt-12">
+      <div className="card border max-w-[900px] mx-auto rounded-xl">
+        <div className="flex items-center ml-12 mt-8 gap-4">
+          <img
+            className="w-24 h-24  rounded-full border-2 border-green-500 p-1"
+            src={user.photoURL}
+            alt=""
+          />
+          <div>
+            <h1 className="font-bold text-2xl">Name: {user?.displayName} </h1>
+            <h2 className="font-bold text-lg text-slate-600">
+              Email: {user?.email}{" "}
+            </h2>
+          </div>
         </div>
-    );
+        <div className="divider px-4"></div>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyProfile;
