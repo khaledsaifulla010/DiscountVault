07b025f4a6bffb5272505edbd3a59472df94d413
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-19 10:58:13 +0600

    card polishing in all brands section

--- src/components/AllBrandCard/AllBrandCard.jsx
+++ src/components/AllBrandCard/AllBrandCard.jsx
@@ -19,44 +19,48 @@ const AllBrandCard = ({ brand }) => {
 
   return (
     <div>
-      <div className="card bg-base-100 w-[400px] h-[480px] border-2 mt-12">
+      <div className="card bg-base-100 w-[400px] h-[480px] border-2 shadow-md hover:shadow-lg transition-shadow duration-300 mt-12 rounded-lg">
         <figure>
-          <img className="w-72 h-40 p-4" src={brand_logo} />
+          <img
+            className="w-72 h-40 p-4 object-contain"
+            src={brand_logo}
+            alt={`${brand_name} logo`}
+          />
         </figure>
         <div className="p-4">
           <div className="flex items-center justify-between">
-            <h2 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-green-600 mt-2">
+            <h2 className="text-4xl font-bold text-purple-800 mt-2">
               {brand_name}
             </h2>
-            <h2 className="text-lg text-pink-600 font-bold  p-2 rounded-lg mt-4 bg-slate-100">
+            <h2 className="text-lg text-pink-600  font-bold rounded-lg mt-4 ">
               {category}
             </h2>
           </div>
           <div className="divider"></div>
-          <p className=" text-xl text-justify   ">{description}</p>
+          <p className="text-base text-gray-700 font-semibold">{description}</p>
 
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between mt-4">
             <Rating
               className="mt-4"
               style={{ maxWidth: 180 }}
               value={rating}
               readOnly
             />
-            <p className="p-2 font-black text-lg mt-2 rounded-xl bg-slate-100">
-              {rating}
+            <p className="p-2 font-black text-lg mt-2 rounded-xl border border-green-100 bg-green-50 text-teal-700">
+              {rating.toFixed(1)}
             </p>
           </div>
 
           <div className="mt-8 flex items-center justify-between">
             <Link
               to={`/brand/${id}`}
-              className="p-2  rounded-xl font-bold text-lg bg-slate-100 text-teal-700"
+              className="p-3 rounded-lg font-bold text-lg bg-blue-100 text-blue-500 border border-blue-200 transition-colors duration-300"
             >
               View Coupons
             </Link>
             {isSaleOn && (
-              <h1 className="font-bold text-red-600 text-lg animate__animated animate__bounce">
-                Sales is on
+              <h1 className="font-bold text-red-600 text-lg animate-bounce">
+                Sale is On!
               </h1>
             )}
           </div>
