bfa8807346539ed9317fd880470e0d491ef450cd
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-19 23:03:24 +0600

    fixes some codes

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -22,12 +22,14 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <div className="flex gap-2 items-center">
-          <img className="w-7 mt-3" src={logo} />
-          <a className=" font-bold text-4xl">
-            Discount<span className="text-orange-500">Vault</span>
-          </a>
-        </div>
+        <Link to={"/"}>
+          <div className="flex gap-2 items-center">
+            <img className="w-7 mt-3" src={logo} />
+            <a className=" font-bold text-4xl">
+              Discount<span className="text-orange-500">Vault</span>
+            </a>
+          </div>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 flex items-center gap-12 font-bold text-lg">
--- src/components/Main/TopBrands/TopBrands.jsx
+++ src/components/Main/TopBrands/TopBrands.jsx
@@ -11,6 +11,7 @@ import image8 from "../../../assets/brands-logo/nike.png";
 import image9 from "../../../assets/brands-logo/Target.png";
 import image10 from "../../../assets/brands-logo/H&M.png";
 import image11 from "../../../assets/brands-logo/ikea.png";
+import { Link } from "react-router-dom";
 
 const TopBrands = () => {
   return (
@@ -20,19 +21,21 @@ const TopBrands = () => {
       </h1>
 
       <div className="mt-12">
-        <Marquee pauseOnHover ={true}>
-          <img className="w-36 mr-12 mt-4" src={image1} />
-          <img className="w-36 mr-12" src={image2} />
-          <img className="w-36 mr-12" src={image3} />
-          <img className="w-36 mr-12" src={image4} />
-          <img className="w-24 rounded-xl h-16 mr-12" src={image5} />
-          <img className="w-36 mr-12" src={image6} />
-          <img className="w-36 mr-12" src={image7} />
-          <img className="w-36 mr-12" src={image8} />
-          <img className="w-36 mr-12" src={image9} />
-          <img className="w-36 mr-12" src={image10} />
-          <img className="w-36 mr-12" src={image11} />
-        </Marquee>
+        <Link to={'/brands'}>
+          <Marquee pauseOnHover={true}>
+            <img className="w-36 mr-12 mt-4" src={image1} />
+            <img className="w-36 mr-12" src={image2} />
+            <img className="w-36 mr-12" src={image3} />
+            <img className="w-36 mr-12" src={image4} />
+            <img className="w-24 rounded-xl h-16 mr-12" src={image5} />
+            <img className="w-36 mr-12" src={image6} />
+            <img className="w-36 mr-12" src={image7} />
+            <img className="w-36 mr-12" src={image8} />
+            <img className="w-36 mr-12" src={image9} />
+            <img className="w-36 mr-12" src={image10} />
+            <img className="w-36 mr-12" src={image11} />
+          </Marquee>
+        </Link>
       </div>
     </div>
   );
