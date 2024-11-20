3caa18a97c00776a9d36a87bda234241da873701
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-20 11:11:20 +0600

    added responsiveness in navbar

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -20,39 +20,41 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 px-4">
       <div className="navbar-start">
         <Link to={"/"}>
           <div className="flex gap-2 items-center">
-            <img className="w-7 mt-3" src={logo} />
-            <a className=" font-bold text-4xl">
+            <img className="w-7 mt-3" src={logo} alt="Logo" />
+            <span className="font-bold text-2xl sm:text-3xl lg:text-4xl">
               Discount<span className="text-orange-500">Vault</span>
-            </a>
+            </span>
           </div>
         </Link>
       </div>
+
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 flex items-center gap-12 font-bold text-lg">
+        <ul className="menu menu-horizontal px-1 flex items-center gap-8 font-bold text-base sm:text-lg">
           <NavLink to={"/"}>Home</NavLink>
           <NavLink to={"/brands"}>Brands</NavLink>
           <NavLink to={"/aboutDev"}>About Dev</NavLink>
         </ul>
       </div>
+
       <div className="navbar-end">
         <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-          <div tabIndex={0} role="button" className="text-4xl m-1">
+          <div tabIndex={0} role="button" className="text-3xl sm:text-4xl m-1">
             {user ? (
               user.photoURL ? (
                 <img
-                  className="w-16 h-16 rounded-full border-2 p-1"
+                  className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-2 p-1"
                   src={user.photoURL}
-                  alt=""
+                  alt="User"
                 />
               ) : (
                 <img
                   src={userIcon}
                   alt="Default User"
-                  className="w-16 h-16 rounded-full border-2 p-1"
+                  className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-2 p-1"
                 />
               )
             ) : (
@@ -61,36 +63,69 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="dropdown-content  menu  rounded-box z-[1] w-48 p-2 h-[200px] shadow border-2 font-bold bg-white "
+            className="dropdown-content menu rounded-box z-[1] w-48 p-2  shadow border-2 font-bold bg-white"
           >
             <li>
               <Link
                 to={"/myProfile"}
-                className="text-lg ml-1 border p-2 rounded-xl text-blue-500 border-blue-300 bg-blue-200 hover:bg-blue-200 mt-3"
+                className="text-base border p-2 rounded-xl text-blue-500 border-blue-300 bg-blue-200 hover:bg-blue-300"
               >
-                <span className="ml-2">My Profile</span> <CgProfile />
+                My Profile <CgProfile />
               </Link>
             </li>
             <li>
               <Link
                 to={"/login"}
-                className="text-lg ml-2 border p-2 rounded-xl text-purple-500 border-purple-300 bg-purple-200 hover:bg-purple-200 mt-2 "
+                className="text-base border p-2 rounded-xl text-purple-500 border-purple-300 bg-purple-200 hover:bg-purple-300 mt-2"
               >
-                <span className="ml-9">Login</span>
-                <FiLogIn />
+                Login <FiLogIn />
               </Link>
             </li>
             <li>
-              {user ? (
+              {user && (
                 <button
                   onClick={hadnleSignOut}
-                  className="text-lg ml-2 border p-2 rounded-xl text-red-500 border-red-300 bg-red-200 hover:bg-red-200 mt-2"
+                  className="text-base border p-2 rounded-xl text-red-500 border-red-300 bg-red-200 hover:bg-red-300 mt-2"
                 >
-                  <span className="ml-9">LogOut</span> <FiLogOut />
+                  LogOut <FiLogOut />
                 </button>
-              ) : (
-                ""
               )}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="navbar-center lg:hidden">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold"
+          >
+            <li>
+              <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/brands"}>Brands</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/aboutDev"}>About Dev</NavLink>
             </li>
           </ul>
         </div>
