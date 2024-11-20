63d2d4d52784ce099bdf9e9b72118b6e629978f6
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-20 11:13:43 +0600

    added responsiveness in banner

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -9,11 +9,43 @@ import slider4 from "../../../assets/slider/slider-4.jpeg";
 const Banner = () => {
   return (
     <div className="mt-12">
-      <Carousel>
-        <img className="rounded-xl" src={slider1} />
-        <img className="rounded-xl" src={slider2} />
-        <img className="rounded-xl" src={slider3} />
-        <img className="rounded-xl" src={slider4} />
+      <Carousel
+        showArrows={true}
+        showThumbs={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={3000}
+        showStatus={false}
+        className="rounded-xl"
+      >
+        <div>
+          <img
+            className="rounded-xl object-cover"
+            src={slider1}
+            alt="Slide 1"
+          />
+        </div>
+        <div>
+          <img
+            className="rounded-xl object-cover"
+            src={slider2}
+            alt="Slide 2"
+          />
+        </div>
+        <div>
+          <img
+            className="rounded-xl object-cover"
+            src={slider3}
+            alt="Slide 3"
+          />
+        </div>
+        <div>
+          <img
+            className="rounded-xl object-cover"
+            src={slider4}
+            alt="Slide 4"
+          />
+        </div>
       </Carousel>
     </div>
   );
